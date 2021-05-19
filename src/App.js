99e44054f06681ae3d9a5866430b71a319615be4
Irving Zamora <irvingzamora28@gmail.com>
2021-05-18 23:13:54 -0600

    Added Posts and Form components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
+import React from 'react';
+import { AppBar, Container, Grid, Grow, Typography } from '@material-ui/core';
+import memories from './images/memories.svg'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
-  );
+const App = () => {
+	const classes = useStyles()
+
+	return (
+
+		<Container maxwidth="lg">
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+				<img className={classes.image} src={memories} alt="memories" height="60" />
+			</AppBar>
+				<Grow in>
+					<Container>
+						<Grid container justify="space-between" alignItems="strech" spacing={3}>
+							<Grid item xs={12} sm={7}>
+								<Posts />
+							</Grid>
+							<Grid item xs={12} sm={4}>
+								<Form />
+							</Grid>
+						</Grid>
+					</Container>
+				</Grow>
+		</Container>
+	)
 }
-
 export default App;
